e4ca1aaae7f54783a2e87c0f4b45545b4a79d0c0
Lucas Holmquist <lholmqui@redhat.com>
2020-10-16 11:58:32 -0400

    chore: just adding the basics

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { CloudEvent } = require('cloudevents');
+
+// a function that takes the github hook and returns a valid cloudevent object
+function parseGithubWebHook (webhook) {
+  // convert the webhook
+
+  // return a cloudevent object
+}
+
+module.exports = parseGithubWebHook;
